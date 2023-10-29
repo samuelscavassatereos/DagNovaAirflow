568447dc9e56d46c3a99979a2e140a274b305ed0
Samuel Scavassa <samuel040103@gmail.com>
2023-10-29 19:42:24 -0300

    git pull

--- DAG.py
+++ DAG.py
@@ -2,19 +2,25 @@ from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
-from extract_api_ecommerce import test, extractCarts, extractProducts, extractUsers
+from airflow.operators.bash import BashOperator
+from DagNovaAirflow.extract_api_ecommerce import test, extractCarts, extractProducts, extractUsers
 
 with DAG(
     'extract_api_test',
     start_date=days_ago(1),
     schedule_interval='@daily'
 ) as dag:
-        
+
+    Sync = BashOperator(
+        task_id= 'git_pull',
+        bash_command='git pull'
+    )
+
     Test = PythonOperator(
         task_id= 'test',
         python_callable=test
     )
-    
+
     Users = PythonOperator(
         task_id= 'users',
         python_callable=extractUsers
@@ -34,4 +40,4 @@ with DAG(
         task_id='success'
     )
 
-Test >> [Users, Carts, Products] >> Success
+Sync >> Test >> [Users, Carts, Products] >> Success
--- extract_api_ecommerce.py
+++ extract_api_ecommerce.py
@@ -1,4 +1,4 @@
-from api_hook import ApiHook
+from DagNovaAirflow.api_hook import ApiHook
 import json as js
 
 
@@ -7,136 +7,110 @@ def test():
     try:
         conn = psycopg2.connect("postgresql://postgres:samuel04@192.168.1.80/postgres")
         print("Conexão bem-sucedida com o banco de dados PostgreSQL")
-        
+
         conn.close()
     except Exception as e:
-        
+
         print(f"Erro ao conectar ao banco de dados: {str(e)}")
-
-
 
 
 
 def extractProducts():
     import psycopg2
-
-    conn = psycopg2.connect(
-            database="postgres",
-            user="postgres",
-            password="samuel04",
-            host="12.168.1.80"
-        )
+    conn = psycopg2.connect("postgresql://postgres:samuel04@192.168.1.80/postgres")
     table_name = 'airflow.products'
     cursor = conn.cursor()
-    
-    try:
-        response = ApiHook.getProducts()
-        for var in response:
-            json = js.loads(var)
-            id = json["id"]   
+
+    #try:
+    response = ApiHook.getProducts()
+    for json in response:
+
+            id = json["id"]
 
             select_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
             cursor.execute(select_query, (id,))
             count = cursor.fetchone()[0]
 
             if count == 0:
-                    
-                    title = json["title"]     
-                    price = json["price"]
-                    category = json["category"]
-                    description = json["description"]
-                    image = json["image"]  
-                    
-                    insert_query = f"INSERT INTO {table_name} (id, title, price, category,
-                    description, image) VALUES (%s, %s, %s, %s, %s, %s)"
-                    cursor.execute(insert_query, (id, title, price, category,
-                    description, image))
-                    conn.commit()
-                    conn.close()
-    except:
-        OSError.add_note('Erro ao extrair produtos')
+                title = json["title"]
+
+                price = json["price"]
+                category = json["category"]
+                description = json["description"]
+                image = json["image"]
+                insert_query = f"INSERT INTO {table_name} (id, title, price, category, descreption, image) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(insert_query, (id, title, price, category, description[:200], image))
+                conn.commit()
+    conn.close()
+
+
+    #except:
+        #print('Erro ao extrair produtos')
 
 
 
-        
 def extractCarts():
     import psycopg2
 
-    conn = psycopg2.connect(
-            database="postgres",
-            user="postgres",
-            password="samuel04",
-            host="12.168.1.80"
-        )
+    conn = psycopg2.connect("postgresql://postgres:samuel04@192.168.1.80/postgres")
     table_name = 'airflow.carts'
     cursor = conn.cursor()
-    
-    try:
-        response = ApiHook.getCarts()
-        for var in response:    
-            json = js.loads(var)
-            id = json["id"]   
 
-            select_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
-            cursor.execute(select_query, (id,))
-            count = cursor.fetchone()[0]
 
-            if count == 0:
-                    
-                    userId = json["userId"]     
+    response = ApiHook.getCarts()
+    for json in response:
+
+        id = json["id"]
+
+        select_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
+        cursor.execute(select_query, (id,))
+        count = cursor.fetchone()[0]
+
+        if count == 0:
+
+                    userId = json["userId"]
                     date = json["date"]
                     products = json["products"]
-                    
+
                     insert_query = f"INSERT INTO {table_name} (id, userId, date, products) VALUES (%s, %s, %s, %s)"
-                    cursor.execute(insert_query, (id, userId, date, products))
+                    cursor.execute(insert_query, (id, userId, date, str(products)))
                     conn.commit()
-                    conn.close()
-    except:
-        OSError.add_note('Erro ao extrair Carts')
+    conn.close()
 
 
 
-        
+
+
 def extractUsers():
     import psycopg2
 
-    conn = psycopg2.connect(
-            database="postgres",
-            user="postgres",
-            password="samuel04",
-            host="12.168.1.80"
-        )
+    conn = psycopg2.connect("postgresql://postgres:samuel04@192.168.1.80/postgres")
     table_name = 'airflow.users'
     cursor = conn.cursor()
-    
-    try:
-        response = ApiHook.getUsers()
-        for var in response:
-            json = js.loads(var)
-            id = json["id"]   
 
-            select_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
-            cursor.execute(select_query, (id,))
-            count = cursor.fetchone()[0]
+    response = ApiHook.getUsers()
+    for json in response:
+        id = json["id"]
+        select_query = f"SELECT COUNT(*) FROM {table_name} WHERE id = %s"
+        cursor.execute(select_query, (id,))
+        count = cursor.fetchone()[0]
+        if count == 0:
 
-            if count == 0:
-                    
-                    email = json["email"]     
+                    email = json["email"]
                     username = json["username"]
                     password = json["password"]
-                    firstname = json["name"]["fisrtname"]
-                    lastname = json["name"]["lastname"]
+                    firstname = json["name"]["fisrt_name"]
+                    lastname = json["name"]["last_name"]
                     city = json["address"]["city"]
                     street = json["address"]["street"]
                     number = json["address"]["number"]
                     zipcode = json["address"]["zipcode"]
-                    phone = json["address"]["phone"]   
-                    
-                    insert_query = f"INSERT INTO {table_name} (id, email, username, password,
-                    fisrtname, lastname, city, street, number, zipcode, phone) VALUES (%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s)"
-                    cursor.execute(insert_query, (id, email, username, password, firstname, lastname, city, street, number, zipcode, phone))
+                    phone = json["address"]["phone"]
+                    lat=None
+                    long=None
+
+                    insert_query = f"INSERT INTO {table_name} (id, email, username, password, first_name, last_name, city, street, number, zipcode, lat, long, phone) VALUES (%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s, %s)"
+                    cursor.execute(insert_query, (int(id), email, username, password, firstname, lastname, city, street, number, zipcode, lat, long, phone))
                     conn.commit()
-                    conn.close()
-    except:
-        print('Erro')
-        
+    conn.close()
+
